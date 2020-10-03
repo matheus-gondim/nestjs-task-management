6e91c32fa11f305a9febadd4dfb78cb172880c5c
matheus-gondim <matheus-pg1@hotmail.com>
2020-10-03 15:36:27 -0300

    Criaçao do service e da model de tasks

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.log(`Listening on port http://localhost:${port}/`);
+  });
 }
 bootstrap();
